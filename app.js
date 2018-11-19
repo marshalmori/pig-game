ecac25eea5b95add807cecbf9703bc1d97ba1982
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-11-19 14:48:40 -0200

    feat: Capturar os pontos obtidos e salvar na pontução total.

--- app.js
+++ app.js
@@ -52,20 +52,34 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     // document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
 
     //SOLUÇÃO DO AUTOR
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
 
   }
 
 });
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  scores[activePlayer] += roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  nextPlayer();
+
+});
+
+
+function nextPlayer(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
