d031602a1f16027b46c53b590e88ed318e733128
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-11-19 11:11:48 -0200

    feat: Iniciar o projeto do jogo Pig Game feito em JavaScript.

---
+++ app.js
@@ -0,0 +1,71 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+// dice = Math.floor(Math.random() * 6) + 1;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<h1>' + dice + '</h1>';
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1){
+    roundScore += dice;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+  }else{
+    //MINHA SOLUÇÃO
+    // roundScore = 0;
+    // document.getElementById('current-' + activePlayer).textContent = '0';
+    // document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    // //Next player
+    // activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    // document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+
+    //SOLUÇÃO DO AUTOR
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+  }
+
+});
